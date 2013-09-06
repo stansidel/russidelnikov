fd069636dfcf44e19dd1eeb60180a4caf4572380
ssidelnikov <stan@eco-nr.ru>
2013-09-06 15:26:07 +0600

    Removed ssidelnikov postfix

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/russidelnikov/"
-http_images_path = "/russidelnikov/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/russidelnikov/fonts"
-css_dir = "public/russidelnikov/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
